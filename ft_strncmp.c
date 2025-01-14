e6eddc9101713d2705bed0f5ec50f3c7adb32c0e
Diana Ortega Suarez <dortega-@car3s5.42barcelona.com>
2025-01-14 14:40:13 +0100

    push

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dortega- <dortega-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 14:21:47 by dortega-          #+#    #+#             */
-/*   Updated: 2025/01/09 15:35:50 by dortega-         ###   ########.fr       */
+/*   Updated: 2025/01/14 12:35:50 by dortega-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] == s2[i] && s1[i])
+	while (i < n && (s1[i] || s2[i]))
 	{
+		if (s1[i] != s2[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		else if (s1[i] == '\0')
+			return (0);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
